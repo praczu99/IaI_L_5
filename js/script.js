f0f996279db1debf09dcadd5ccd4744c92ac5259
praczu99 <72567448+praczu99@users.noreply.github.com>
2020-11-03 10:03:50 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+
+var result = '\n';
+for (var i = 1; i < 11; i++) {
+
+    for (var j = 1; j < 11; j++) {
+        result += (i*j) + ' ';
+
+    }
+    result += '\n'
+}
+
+console.log(result);
